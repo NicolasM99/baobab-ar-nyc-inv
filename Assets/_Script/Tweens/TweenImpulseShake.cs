633acb8b017134e26636bef49b350b7b4864fd29
Alimen6 <alejoncho98@hotmail.com>
2024-02-28 18:33:23 -0500

    Update TweenImpulseShake.cs

--- Assets/_Script/Tweens/TweenImpulseShake.cs
+++ Assets/_Script/Tweens/TweenImpulseShake.cs
@@ -9,11 +9,11 @@ public class TweenImpulseShake : MonoBehaviour
     [SerializeField] float shakeTime;
     [SerializeField] int loopCount;
 
-    Vector3 defaultPosition;
+    Vector3 startingScale;
 
     private void Start()
     {
-        defaultPosition = gameObject.transform.position;
+        startingScale = gameObject.transform.localScale;
     }
 
     public void ShakeSelf()
@@ -22,8 +22,10 @@ public class TweenImpulseShake : MonoBehaviour
         float amountToShakeY = Random.Range(minAmountToShake.y, maxAmountToShake.y);
         float amountToShakeZ = Random.Range(minAmountToShake.z, maxAmountToShake.z);
 
-        LeanTween.moveY(gameObject, gameObject.transform.position.y - amountToShakeY, shakeTime).setLoopCount(loopCount);
-        LeanTween.moveZ(gameObject, gameObject.transform.position.z - amountToShakeZ, shakeTime).setLoopCount(loopCount);
-        LeanTween.moveX(gameObject, gameObject.transform.position.x - amountToShakeX, shakeTime).setLoopCount(loopCount).setOnComplete(() => LeanTween.move(gameObject, defaultPosition, shakeTime));
+        Vector3 targetScale = new Vector3(gameObject.transform.localScale.x + amountToShakeX,
+            transform.localScale.y + amountToShakeY,
+            transform.localScale.z + amountToShakeZ);
+
+        LeanTween.scale(gameObject, targetScale, shakeTime).setOnComplete(() => LeanTween.scale(gameObject, startingScale, shakeTime));   
     }
 }
